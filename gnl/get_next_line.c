e65305a3357891ee1d12669ed748f4a9541b5dba
Gian Brandon stark <gbrandon@e1r2p2.21-school.ru>
2018-12-19 16:38:28 +0300

    add experemental gnl with intermediate buffer

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -64,40 +64,45 @@ static int		create_line(char **buf, char **line)
 
 int				get_next_line(const int fd, char **line)
 {
-	static char *buf = NULL;
+	char		 *buf;
 	int			rbytes;
 	char		*head_buf;
 
 	size_t		inter_buf_size; ///
-	char		*inter_buf; ///
+	static char		*inter_buf; ///
+	char		*inter_head_buf; ///
 
 	CHECK_CMP_ERR(fd < 0);
 	CHECK_ERR(line);
 	*line = NULL;
 	rbytes = 0;
-	inter_buf_size = BUFF_SIZE; ///
-	CHECK_ERR(inter_buf = (char*)malloc(sizeof(char) * (INTERMEDIATE_SIZE + 1))); ///
-	if (buf)
-		CHECK_CMP_ERR((rbytes = create_line(&buf, line)) < 0);
+	inter_buf_size = 0; ///
+	if (inter_buf)
+		CHECK_CMP_ERR((rbytes = create_line(&inter_buf, line)) < 0);
 	IF_RET_ONE(rbytes == 1);
+	CHECK_ERR(inter_buf = (char*)malloc(sizeof(char) * (INTERMEDIATE_SIZE + 1))); ///
 	CHECK_ERR(buf = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1)));
+	inter_head_buf = inter_buf; ///
 	head_buf = buf;
 	while ((rbytes = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		if (inter_buf_size < INTERMEDIATE_SIZE)
+		buf[rbytes] = '\0';
+		while (*buf && inter_buf_size < INTERMEDIATE_SIZE)
+			inter_buf[inter_buf_size++] = *buf++;
+		if (inter_buf_size < INTERMEDIATE_SIZE && *head_buf)
 		{
-			*inter_buf = *buf;
-			inter_buf_size += BUFF_SIZE;
-			inter_buf++;
+			buf = head_buf;
 			continue;
 		}
-		buf[rbytes] = '\0';
-		CHECK_CMP_ERR((rbytes = create_line(&buf, line)) < 0);
+		inter_buf[inter_buf_size] = '\0';
+		CHECK_CMP_ERR((rbytes = create_line(&inter_buf, line)) < 0);
 		IF_RET_ONE(rbytes == 1);
+		inter_buf = inter_head_buf;
 		buf = head_buf;
 	}
 	CHECK_CMP_ERR(rbytes < 0);
 	FREE_X(buf);
+	FREE_X(inter_buf);
 	IF_RET_ONE(*line);
 	return (0);
 }
