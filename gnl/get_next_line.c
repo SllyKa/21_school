5c7c6362c13a5e99513646bacdb261a4f565b12e
Gian Brandon stark <gbrandon@e1r2p4.21-school.ru>
2018-12-21 21:21:37 +0300

    old buffer

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gbrandon <gbrandon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/07 13:45:41 by gbrandon          #+#    #+#             */
-/*   Updated: 2018/12/07 15:29:18 by gbrandon         ###   ########.fr       */
+/*   Updated: 2018/12/20 15:47:21 by gbrandon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,73 @@
 #include "libft.h"
 #include "get_next_line.h"
 
-static int		assembly_line(char **hli, char **buf, char **line, size_t len)
+static t_list				*get_fd(t_list **head, int fd)
+{
+	t_info		*new;
+	t_list		*new_head;
+
+	new_head = *head;
+	while (new_head)
+	{
+		if (((t_info*)new_head->content)->fd == fd)
+			return (new_head);
+		new_head = new_head->next;
+	}
+	if (!(new = (t_info*)malloc(sizeof(t_info))))
+		return (NULL);
+	new->fd = fd;
+	new->buf = NULL;
+	new->inter_buf = NULL;
+	new->buf_size = 0;
+	new->i_buf_size = 0;
+	new->buf_cur_pos = 0;
+	if (!(new_head = ft_lstnew((const void*)new, 0)))
+		return (NULL);
+	if (!(new_head->content = new))
+	{
+		free(new_head);
+ 		return (NULL);
+ 	}
+ 	new_head->next = NULL;
+	ft_lstadd(head, new_head);
+	return (*head);
+}
+
+static int					assembly_line(char **hli, t_info *cbuf, char **line)
 {
 	char		*temp;
-	size_t		rest_len;
 
-	CHECK_ERR(temp = ft_strsub(*hli, 0, *buf - *hli));
+	CHECK_ERR(temp = ft_strsub(*hli, 0, cbuf->inter_buf - *hli));
 	if (*line)
 	{
 		CHECK_ERR(*line = ft_strjoin(*line, temp));
 	}
 	else
 		*line = temp;
-	if ((rest_len = len - (*buf - *hli)) > 1)
+	if ((cbuf->i_buf_size = cbuf->i_buf_size - (cbuf->inter_buf - *hli)) > 1)
 	{
-		CHECK_ERR(*buf = ft_strsub(*buf, 1, len - (*buf - *hli)));
+		CHECK_ERR(cbuf->inter_buf = ft_strsub(cbuf->inter_buf, 1, cbuf->i_buf_size));
+		cbuf->i_buf_size = cbuf->i_buf_size - 1;
 	}
 	else
-		*buf = NULL;
-	free(*hli);
-	*hli = NULL;
+		cbuf->i_buf_size = 0;
 	return (1);
 }
 
-static int		create_line(char **buf, char **line)
+static int					create_line(t_info *cur_buf, char **line)
 {
 	char	*head_line;
-	size_t	line_length;
 
-	head_line = *buf;
-	line_length = ft_strlen(*buf);
-	while (**buf)
+	head_line = cur_buf->inter_buf;
+	//cur_buf->i_buf_size = ft_strlen(head_line); //?
+	while (*cur_buf->inter_buf)
 	{
-		if (**buf == '\n')
+		if (*(cur_buf->inter_buf) == '\n')
 		{
-			CHECK_ERR(assembly_line(&head_line, buf, line, line_length));
+			CHECK_ERR(assembly_line(&head_line, cur_buf, line));
 			return (1);
 		}
-		(*buf)++;
+		(cur_buf->inter_buf)++;
 	}
 	if (*line)
 	{
@@ -59,50 +88,73 @@ static int		create_line(char **buf, char **line)
 	}
 	else
 		CHECK_ERR(*line = ft_strdup(head_line));
+	cur_buf->i_buf_size = 0;
+	cur_buf->inter_buf = head_line;
 	return (0);
 }
 
-int				get_next_line(const int fd, char **line)
+static int					proc(t_info *cur_buf, char **line)
 {
-	char		 *buf;
-	int			rbytes;
-	char		*head_buf;
+	int				err_code;
 
-	size_t		inter_buf_size; ///
-	static char		*inter_buf; ///
-	char		*inter_head_buf; ///
+	err_code = 0;
+	while (cur_buf->buf_size > 0 || cur_buf->i_buf_size > 0)
+	{
+		while (cur_buf->buf_size > 0 && cur_buf->i_buf_size < INTERMEDIATE_SIZE)
+		{
+			cur_buf->inter_buf[cur_buf->i_buf_size++] = cur_buf->buf[cur_buf->buf_cur_pos++];
+			cur_buf->buf_size--;
+		}
+		if (cur_buf->i_buf_size < INTERMEDIATE_SIZE && cur_buf->buf_size > 0)
+		{
+			cur_buf->buf_cur_pos = 0;
+			break;
+		}
+		cur_buf->inter_buf[cur_buf->i_buf_size] = '\0';
+		CHECK_CMP_ERR((err_code = create_line(cur_buf, line)) < 0);
+		IF_BREAK(err_code == 1);
+	}
+	return (err_code);
+}
+
+int							get_next_line(const int fd, char **line)
+{
+	static t_list	*list_head = NULL;
+	t_info			*cur_buf;
+	int				rbytes;
+	char			*inter_buf_head;
 
 	CHECK_CMP_ERR(fd < 0);
 	CHECK_ERR(line);
 	*line = NULL;
 	rbytes = 0;
-	inter_buf_size = 0; ///
-	if (inter_buf)
-		CHECK_CMP_ERR((rbytes = create_line(&inter_buf, line)) < 0);
-	IF_RET_ONE(rbytes == 1);
-	CHECK_ERR(inter_buf = (char*)malloc(sizeof(char) * (INTERMEDIATE_SIZE + 1))); ///
-	CHECK_ERR(buf = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1)));
-	inter_head_buf = inter_buf; ///
-	head_buf = buf;
-	while ((rbytes = read(fd, buf, BUFF_SIZE)) > 0)
+	CHECK_ERR(cur_buf = get_fd(&list_head, fd)->content);
+	if (cur_buf->i_buf_size > 0 || cur_buf->buf_size > 0)
 	{
-		buf[rbytes] = '\0';
-		while (*buf && inter_buf_size < INTERMEDIATE_SIZE)
-			inter_buf[inter_buf_size++] = *buf++;
-		if (inter_buf_size < INTERMEDIATE_SIZE && *head_buf)
-		{
-			buf = head_buf;
-			continue;
-		}
-		inter_buf[inter_buf_size] = '\0';
-		CHECK_CMP_ERR((rbytes = create_line(&inter_buf, line)) < 0);
-		IF_RET_ONE(rbytes == 1);
-		inter_buf = inter_head_buf;
-		buf = head_buf;
+		ERR_PROC(proc(cur_buf, line));
 	}
+	if (!cur_buf->inter_buf && !cur_buf->buf)
+	{
+		CHECK_ERR(cur_buf->inter_buf = (char*)malloc(sizeof(char) * (INTERMEDIATE_SIZE + 1))); ///
+		CHECK_ERR(cur_buf->buf = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1)));
+	}
+	inter_buf_head = cur_buf->inter_buf;
+	while (((rbytes = read(fd, cur_buf->buf, BUFF_SIZE)) > 0) ||
+	(rbytes == 0 && (cur_buf->buf_size > 0 || cur_buf->i_buf_size > 0))) ///?
+	{
+		cur_buf->buf_cur_pos = 0;
+		cur_buf->buf[rbytes] = '\0';
+		cur_buf->buf_size = rbytes;
+		rbytes = proc(cur_buf, line);
+		if (cur_buf->i_buf_size == 0)
+			cur_buf->inter_buf = inter_buf_head;
+		ERR_PROC(rbytes);
+	}
+	cur_buf->inter_buf = inter_buf_head;
 	CHECK_CMP_ERR(rbytes < 0);
-	FREE_X(buf);
-	FREE_X(inter_buf);
+	FREE_X(cur_buf->buf);
+	FREE_X(cur_buf->inter_buf);
+	/// free list
 	IF_RET_ONE(*line);
 	return (0);
 }
--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -13,13 +13,25 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1  // -23 0 ?
-# define INTERMEDIATE_SIZE 700
-# define CHECK_ERR(x) if (!(x)) return (-1)
-# define CHECK_CMP_ERR(x) if ((x)) return (-1)
-# define IF_RET_ONE(x) if ((x)) return (1)
+# define BUFF_SIZE 263//19
+# define INTERMEDIATE_SIZE 700//4
+# define ret(x) return (x)
+# define CHECK_ERR(x) if (!(x)) ret(-1)
+# define CHECK_CMP_ERR(x) if ((x)) ret (-1)
+# define IF_RET_ONE(x) if ((x)) ret (1)
 # define FREE_X(x) if ((x)){free((x)); (x) = NULL;}
+# define ERR_PROC(x) if ((x)==1) ret(1); else if ((x)==-1) ret(-1);
+# define IF_BREAK(x) if ((x)) break
 
-int         get_next_line(const int fd, char **line);
+typedef struct			s_info
+{
+	int			fd;
+	char		*buf;
+	size_t		buf_size;
+	char		*inter_buf;
+	size_t		i_buf_size;
+	size_t		buf_cur_pos;
+}						t_info;
+int						get_next_line(const int fd, char **line);
 
 #endif
